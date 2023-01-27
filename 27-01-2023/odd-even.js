ffce5fdf5dd1152d8d143fcae2110646f4276d93
Kakon Debnath <kakondebnath96@gmail.com>
2023-01-27 21:35:19 +0600

    odd and even number

---
+++ 27-01-2023/odd-even.js
@@ -0,0 +1,13 @@
+// Display 0-100 even number
+// var number = 0;
+// while (number <= 100) {
+//     console.log(number);
+//     number = number +2;
+// }
+
+// Display 1-99 odd number
+var oddNumber = 1;
+while(oddNumber <= 99 ){
+    console.log(oddNumber);
+    oddNumber = oddNumber + 2;
+}
