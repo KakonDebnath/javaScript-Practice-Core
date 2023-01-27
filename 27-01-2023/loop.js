16abcd9b99828aacdf9be608f9617a561b9f180c
Kakon Debnath <kakondebnath96@gmail.com>
2023-01-27 22:14:13 +0600

    for loop array

--- 27-01-2023/loop.js
+++ 27-01-2023/loop.js
@@ -6,11 +6,23 @@
     3. loop body
     4. loop variable increase  or decrease
 */
+// While loop
+// var number = 1;
+// while (number <= 100 ) {
+//     console.log(number);
+//     // number = number + 1;
+//     // number += 1;
+//     number++;
+// }
 
-var number = 1;
-while (number <= 100 ) {
-    console.log(number);
-    // number = number + 1;
-    // number += 1;
-    number++;
+// For loop
+/* 
+for (var i = 1; i<=20; i= i+2){
+    console.log(i);
+} */
+
+var players = ["liton", "sakib", "miraz", "bijoy", "santo", "musfiq", "mustafizur"];
+var player; 
+for (i = 0; i < players.length; i++){
+    console.log(player = players[i] );
 }
