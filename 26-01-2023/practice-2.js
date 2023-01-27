ee5e9b4740b0231111dd6ff3b27579cc1b09785b
Kakon Debnath <kakondebnath96@gmail.com>
2023-01-27 13:30:13 +0600

    multi-stage condition

---
+++ 26-01-2023/practice-2.js
@@ -0,0 +1,106 @@
+// Practice -2 grading system
+// Marks
+// var kakon = 85;
+// var tom = 66;
+// var jane = 95;
+// var peter = 56;
+// var jon = 40;
+
+var names = ["Kakon", "Emon", "Rasel", "kobir", "Raju"];
+var marks = [85, 66, 95, 56, 40];
+// grading Table 
+//  80 or above A
+//  60 or above B
+//  50 or above C
+//  40 or above D
+//  39 or Less F
+
+// array first element
+// if (marks[0] >= 80 ) {
+//     console.log("congratulation " + names[0] + " you got 'A' Grade ");
+// }
+if (marks[0] >= 80 ) {
+    console.log("congratulation " + names[0] + " you got 'A' Grade ");
+}
+else if (marks[0] >= 60) {
+    console.log("congratulation " + names[0] + " you got 'B' Grade ");
+}
+else if (marks[0] >= 50) {
+    console.log("congratulation " + names[0] + " you got 'C' Grade ");
+}
+else if (marks[0] >= 40) {
+    console.log("congratulation " + names[0] + " you got 'D' Grade ");
+}
+else {
+    console.log("Sorry " + names[0] + " you are fail the exam ");
+}
+
+// array second element
+if (marks[1] >= 80 ) {
+    console.log("congratulation " + names[1] + " you got 'A' Grade ");
+}
+else if (marks[1] >= 60) {
+    console.log("congratulation " + names[1] + " you got 'B' Grade ");
+}
+else if (marks[1] >= 50) {
+    console.log("congratulation " + names[1] + " you got 'C' Grade ");
+}
+else if (marks[1] >= 40) {
+    console.log("congratulation " + names[1] + " you got 'D' Grade ");
+}
+else {
+    console.log("Sorry " + names[1] + " you are fail the exam ");
+}
+
+// array third element
+if (marks[2] >= 80 ) {
+    console.log("congratulation " + names[2] + " you got 'A' Grade ");
+}
+else if (marks[2] >= 60) {
+    console.log("congratulation " + names[2] + " you got 'B' Grade ");
+}
+else if (marks[2] >= 50) {
+    console.log("congratulation " + names[2] + " you got 'C' Grade ");
+}
+else if (marks[2] >= 40) {
+    console.log("congratulation " + names[2] + " you got 'D' Grade ");
+}
+else {
+    console.log("Sorry " + names[2] + " you are fail the exam ");
+}
+
+// array fourth element
+
+if (marks[3] >= 80 ) {
+    console.log("congratulation " + names[3] + " you got 'A' Grade ");
+}
+else if (marks[3] >= 60) {
+    console.log("congratulation " + names[3] + " you got 'B' Grade ");
+}
+else if (marks[3] >= 50) {
+    console.log("congratulation " + names[3] + " you got 'C' Grade ");
+}
+else if (marks[3] >= 40) {
+    console.log("congratulation " + names[3] + " you got 'D' Grade ");
+}
+else {
+    console.log("Sorry " + names[3] + " you are fail the exam ");
+}
+
+// array fifth element
+
+if (marks[4] >= 80 ) {
+    console.log("congratulation " + names[4] + " you got 'A' Grade ");
+}
+else if (marks[4] >= 60) {
+    console.log("congratulation " + names[4] + " you got 'B' Grade ");
+}
+else if (marks[4] >= 50) {
+    console.log("congratulation " + names[4] + " you got 'C' Grade ");
+}
+else if (marks[4] >= 40) {
+    console.log("congratulation " + names[4] + " you got 'D' Grade ");
+}
+else {
+    console.log("Sorry " + names[0] + " you are fail the exam ");
+}
