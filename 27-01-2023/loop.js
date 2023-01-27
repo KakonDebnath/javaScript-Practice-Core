77d2aaa3b1bb65acea1f336abd4e6fbfe2ba587a
Kakon Debnath <kakondebnath96@gmail.com>
2023-01-27 21:25:25 +0600

    add defination

--- 27-01-2023/loop.js
+++ 27-01-2023/loop.js
@@ -1,3 +1,12 @@
+/*
+    a loop should must 4 part
+    -------------------------
+    1. loop initial 
+    2. condition
+    3. loop body
+    4. loop variable increase  or decrease
+*/
+
 var number = 1;
 while (number <= 100 ) {
     console.log(number);
