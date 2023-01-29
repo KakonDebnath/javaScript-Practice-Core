0b340e82e286970c5704c9b331147b35171b59de
Kakon <pronobparsonal@gmail.com>
2023-01-29 13:44:59 +0600

    practice office

---
+++ 29-01-2023/index.js
@@ -0,0 +1,21 @@
+// var number = 20;
+// var number2 = "20.52";
+// var convertedNumber = parseFloat(number2);
+// var fixedNumber = convertedNumber.toFixed(1);
+// console.log(typeof(fixedNumber));
+// console.log(convertedNumber);
+
+// console.log(typeof(number));
+// console.log(typeof(number2));
+// console.log(typeof(parseFloat(number2)));
+// console.log(typeof(convertedNumber.toFixed(1)));
+
+
+var color = "yellow";
+if(color == "red"){
+      console.log("Stopped");
+}else if(color == "yellow"){
+      console.log("Wait sometime");
+}else{
+      console.log("You Can GO now");
+}
