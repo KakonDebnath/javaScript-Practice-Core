8b33fb9b64428ebd28b950fe429a10ed512f7c62
Kakon <pronobparsonal@gmail.com>
2023-01-30 14:18:11 +0600

    switch

---
+++ 30-01-2023/switch.js
@@ -0,0 +1,31 @@
+let a = "";
+
+switch (a) {
+      case 1:
+            a = 'Today Is Saturday';
+            break;
+      case 2:
+            a = 'Today Is Sunday';
+            break;
+      case 3:
+            a = 'Today Is Monday';
+            break;
+      case 4:
+            a = 'Today Is Tuesday';
+            break;
+      case 5:
+            a = 'Today Is Wednesday';
+            break;
+      case 6:
+            a = 'Today Is Thursday';
+            break;
+      case 7:
+            a = 'Today Is Friday';
+            break;
+
+      default:
+            a = 'not found';
+            break;
+}
+
+console.log(a);
