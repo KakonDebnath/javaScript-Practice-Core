6477402c87dca4cc27f7245e817f645cec545c77
Kakon <pronobparsonal@gmail.com>
2023-01-31 10:23:31 +0600

    odd-even number

---
+++ 31-01-2023/index.js
@@ -0,0 +1,21 @@
+/*
+ // Odd-Even Number if- else Statement
+
+function oddNumber(number) {
+      let msg = "";
+      if (number % 2 === 0) {
+            return msg += "This Is Even Number";
+      }
+      else{
+            return msg += "This Is Odd Number";
+      }
+}
+
+let number = 25;
+let myOddNumber = oddNumber(number);
+console.log(myOddNumber);
+ */
+
+
+
+
