608bee7e7701510f84738a9cbbc168133a4ee982
Kakon <pronobparsonal@gmail.com>
2023-01-31 10:53:44 +0600

    avg number

--- 31-01-2023/index.js
+++ 31-01-2023/index.js
@@ -17,5 +17,24 @@ console.log(myOddNumber);
  */
 
 
+/* function add(numbers) {
+      let result = 0;
+      for (let i = 0; i < numbers.length; i++) {
+            result += numbers[i];
+      }
+      return result;
+}
+function avg(numbers) {
+      let result = 0;
+      let arrayLength = numbers.length;
+      let sum = add(numbers);
+      result += sum / arrayLength;
+      return result;
+}
+
+let numbers = [10, 20, 30, 40, 50, 60, 90];
+let avgNumber = avg(numbers);
+ */
+
 
 
