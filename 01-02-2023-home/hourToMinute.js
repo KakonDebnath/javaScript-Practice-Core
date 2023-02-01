4858e5667ac2888aa0b68050963b4f9de9a7013f
Kakon Debnath <kakondebnath96@gmail.com>
2023-02-01 18:03:49 +0600

    done

---
+++ 01-02-2023-home/hourToMinute.js
@@ -0,0 +1,12 @@
+function hourToMin(hour){
+    //write your code here
+    //don't forget to write return
+    const minute = hour * 60;
+    return minute;
+
+}
+
+
+let hour = 1;
+const minute = hourToMin(hour);
+console.log(minute);
