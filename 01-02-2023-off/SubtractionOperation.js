4cbbad223b4fe5259d7e95d12fbf44af82a6287a
Kakon <pronobparsonal@gmail.com>
2023-02-01 14:11:52 +0600

    done

---
+++ 01-02-2023-off/SubtractionOperation.js
@@ -0,0 +1,8 @@
+function subtraction(num1, num2) {
+    let subtractionNum = 0;
+    subtractionNum = num1 - num2;
+    return subtractionNum;
+}
+
+let sub = subtraction(1000, 700);
+console.log(sub);
