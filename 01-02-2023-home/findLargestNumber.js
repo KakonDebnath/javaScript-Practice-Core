8ad8e95f2d9b82780bca6a7180920ed89682cad2
Kakon Debnath <kakondebnath96@gmail.com>
2023-02-01 21:29:27 +0600

    second Leargest Number

--- 01-02-2023-home/findLargestNumber.js
+++ 01-02-2023-home/findLargestNumber.js
@@ -1,4 +1,4 @@
-function findMax(arrOfNumbers) {
+/* function findMax(arrOfNumbers) {
     let largestNumber = 0;
     for (let i = 0; i < arrOfNumbers.length; i++) {
 
@@ -14,3 +14,22 @@ function findMax(arrOfNumbers) {
 let numbers = [20, 85, 71, 60, 90, 150];
 const largeNumber = findMax(numbers);
 console.log("Your Large Number Is: " +  largeNumber);
+ */
+
+function findSecondLargestElem(arr){
+    let first = -1 , second = -1;
+
+    for(let i = 0; i <= arr.length; i++){
+        if(arr[i] > first){
+            second = first;
+            first = arr[i];
+        }
+        else if( arr[i] > second && arr[i] != first){
+            second = arr[i];
+        }
+    }
+    console.log(first);
+    console.log(second);
+}
+let arr = [12, 35, 1, 10, 34, 1,39]
+findSecondLargestElem(arr);
