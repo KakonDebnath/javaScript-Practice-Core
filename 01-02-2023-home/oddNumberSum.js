06d20e843c2ffd6d4621b5932dbf7911edce6732
Kakon Debnath <kakondebnath96@gmail.com>
2023-02-01 19:33:50 +0600

    done 2nd Method

--- 01-02-2023-home/oddNumberSum.js
+++ 01-02-2023-home/oddNumberSum.js
@@ -1,4 +1,5 @@
 
+/* 
 // Method-1
 function findOddSum(arrOfNumbers){
     // Find Odd Number
@@ -18,7 +19,34 @@ function findOddSum(arrOfNumbers){
         result += totalNumber;
     }
     return result;
+} 
+*/
+
+
+
+// Method-2
+function findOddNumber(arr){
+    let oddNumber = [];
+    for (let i = 0; i < arr.length; i ++){
+        const numbers = arr[i];
+        if (numbers % 2 !==0) {
+            oddNumber.push(numbers);
+        }
+    } 
+    return oddNumber;
 }
+
+function findOddSum(arrOfNumbers){
+    // Sum Odd Number
+    const oddNumber  = findOddNumber(arrOfNumbers);
+    let result = 0;
+    for(let i = 0; i < oddNumber.length; i++){
+        const totalNumber = oddNumber[i];
+        result += totalNumber;
+    }
+    return result;
+} 
+
 
 let numbers = [5, 7, 8, 10, 45, 30];
 const oddNumberSum = findOddSum (numbers);
