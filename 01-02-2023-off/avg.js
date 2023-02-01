206c43ddab8cda190a53291c70b7a99837c48900
Kakon <pronobparsonal@gmail.com>
2023-02-01 14:28:23 +0600

    another Example

--- 01-02-2023-off/avg.js
+++ 01-02-2023-off/avg.js
@@ -17,6 +17,20 @@ function average(arrOfMarks) {
 }
 
 
+/* function average(arrOfMarks) {
+    let sumOfSub = 0;
+    for (let i = 0; i < arrOfMarks.length; i++) {
+        sumOfSub += arrOfMarks[i];
+    }
+    let myAvg = sumOfSub / arrOfMarks.length;
+    myAvg = myAvg.toFixed(2);
+    myAvg = parseFloat(myAvg);
+    return myAvg;
+} */
+
+
+
+
 let marks = [75.25, 65, 80, 35.45, 99.50];
 let avg = average(marks);
 console.log(avg);
