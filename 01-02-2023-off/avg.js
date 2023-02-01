d86e146405371afdb9c9d86da78cb35269077141
Kakon <pronobparsonal@gmail.com>
2023-02-01 14:23:50 +0600

    done

---
+++ 01-02-2023-off/avg.js
@@ -0,0 +1,22 @@
+function sumOfArray(array) {
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result;
+}
+
+function average(arrOfMarks) {
+    //write your code here
+    // don't forget to write return
+    let sumOfSub = sumOfArray(arrOfMarks);
+    let myAvg = sumOfSub / arrOfMarks.length;
+    myAvg = myAvg.toFixed(2);
+    myAvg = parseFloat(myAvg);
+    return myAvg;
+}
+
+
+let marks = [75.25, 65, 80, 35.45, 99.50];
+let avg = average(marks);
+console.log(avg);
