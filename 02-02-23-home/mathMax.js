cca47f03e3c3a80164810a09696f11e9472b3a2d
Kakon Debnath <kakondebnath96@gmail.com>
2023-02-02 21:42:57 +0600

    done for array

---
+++ 02-02-23-home/mathMax.js
@@ -0,0 +1,7 @@
+// const arr = [1, 2, 3, 50];
+
+// console.log(Math.max.apply(null, arr));
+
+// const arr = [1, 2, 3, 80, 90, 94, 14, 62, 72];
+// const max = Math.max(...arr);
+// console.log(max);
