59e8afbb32e6271c119549c47221227bcce8deec
Kakon Debnath <kakondebnath96@gmail.com>
2023-02-02 21:52:53 +0600

    done

---
+++ 02-02-23-home/mathRandom.js
@@ -0,0 +1,16 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt(1000));
+  // Expected output: 100-999
+  
+  console.log(getRandomInt(100));
+  // Expected output: 10-99
+  
+  console.log(getRandomInt(10));
+  // Expected output: 0-9
+  
+  console.log(Math.random());
+  // Expected output: a number from 0 to <1
+  
