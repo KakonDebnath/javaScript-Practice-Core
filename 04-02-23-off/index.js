b6f1b7751d5633abc77c2f31435506566d78ded3
Kakan Chandra <121675997+KakonDebnath@users.noreply.github.com>
2023-02-04 09:19:05 +0600

    Add files via upload

---
+++ 04-02-23-off/index.js
@@ -0,0 +1,23 @@
+
+function getYoungestFriend (friends) {
+    let youngestFriend = friends[0];
+    for (let i = 0; i<friends.length; i++){
+        if (friends[0].age > friends[i].age ) {
+            youngestFriend = friends[i];
+        }
+    }
+    return youngestFriend;
+}
+
+const friends = [
+    {name : "habib", age: 20,},
+    {name : "Rakib", age: 25,},
+    {name : "Labib", age: 29,},
+    {name : "Labib", age: 19,},
+];
+
+const youngestFriend = getYoungestFriend(friends);
+console.log(youngestFriend);
+
+
+
