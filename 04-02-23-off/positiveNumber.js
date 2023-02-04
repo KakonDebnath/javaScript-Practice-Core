efc3f56cb996bb2076e306b03336354f1a6be8bb
Kakon Debnath <kakondebnath96@gmail.com>
2023-02-04 22:17:33 +0600

    positive nUmberDOne

---
+++ 04-02-23-off/positiveNumber.js
@@ -0,0 +1,19 @@
+// ৫. এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে। 
+
+
+function positiveNumber(numbers) {
+    let positiveNumber = [];
+    for (let i = 0; i<numbers.length; i++ ) {
+        if (numbers[i] > 0 ){
+            positiveNumber.push(numbers[i]);
+        }
+        else {
+            break;
+        }
+    }
+    return positiveNumber;
+}
+
+const numbers = [20, 28, 51, -52, 63, 74, -12];
+const pNumber = positiveNumber (numbers);
+console.log(pNumber);
