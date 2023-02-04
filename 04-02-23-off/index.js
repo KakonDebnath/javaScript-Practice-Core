d03318fd901e8f9fee42fd2bb84aaaa42dd7633d
Kakon Debnath <kakondebnath96@gmail.com>
2023-02-04 22:59:41 +0600

    done

--- 04-02-23-off/index.js
+++ 04-02-23-off/index.js
@@ -1,23 +1,39 @@
 
-function getYoungestFriend(friends) {
-    let youngestFriend = friends[0];
-    for (let i = 0; i < friends.length; i++) {
-        if (friends[0].age > friends[i].age) {
-            youngestFriend = friends[i];
-        }
+// function getYoungestFriend(friends) {
+//     let youngestFriend = friends[0];
+//     for (let i = 0; i < friends.length; i++) {
+//         if (friends[0].age > friends[i].age) {
+//             youngestFriend = friends[i];
+//         }
+//     }
+//     return youngestFriend;
+// }
+
+// const friends = [
+//     { name: "habib", age: 20, },
+//     { name: "Rakib", age: 25, },
+//     { name: "Labib", age: 29, },
+//     { name: "Labib", age: 19, },
+// ];
+
+// const youngestFriend = getYoungestFriend(friends);
+// console.log(youngestFriend);
+
+function JimOrDelaOrChinku(marksOfJim, marksOfDela, marksOfChinku) {
+    //Write your code here
+    //Don't forget to return
+    if (marksOfJim > marksOfDela && marksOfJim > marksOfChinku) {
+        return "Jim";
+    } else if (marksOfDela > marksOfChinku && marksOfDela > marksOfJim) {
+        return "Dela";
+    } else {
+        return "Chinku";
     }
-    return youngestFriend;
 }
 
-const friends = [
-    { name: "habib", age: 20, },
-    { name: "Rakib", age: 25, },
-    { name: "Labib", age: 29, },
-    { name: "Labib", age: 19, },
-];
 
-const youngestFriend = getYoungestFriend(friends);
-console.log(youngestFriend);
-
-
-
+const marksOfChinku = 77;
+const marksOfDela = 99;
+const marksOfJim = 84;
+const topper = JimOrDelaOrChinku(marksOfChinku, marksOfDela, marksOfJim);
+console.log(topper);
