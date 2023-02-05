bb354554ab8f0b7e7283137bfa954e955324b914
Kakon <pronobparsonal@gmail.com>
2023-02-05 13:14:27 +0600

    done odd Even num sum

--- 05-02-2023/index.js
+++ 05-02-2023/index.js
@@ -1,6 +1,6 @@
-// 1-100 Number Sum
+// 1-10 Number Sum
 
-function sum() {
+/* function sum() {
     let sum = 0;
     for (let i = 1; i <= 10; i++) {
         sum = sum + i;
@@ -9,4 +9,29 @@ function sum() {
 }
 
 const totalSum = sum ();
-console.log(totalSum);
+console.log(totalSum); */
+
+
+/* 
+    // 1-100 even number sum
+    function getEvenNumber() {
+        let evenNumberSum = 0;
+        for (let i = 0; i <= 100; i = i + 2) {
+            evenNumberSum += i;
+        }
+        return evenNumberSum;
+    }
+    const result = getEvenNumber();
+    console.log(result); 
+*/
+// 1-100 even number sum
+function getOddNumber() {
+    let oddNumberSum = 0;
+    for (let i = 1; i <= 100; i = i + 2) {
+        // console.log(i);
+        oddNumberSum += i;
+    }
+    return oddNumberSum;
+}
+const result = getOddNumber();
+console.log(result);
