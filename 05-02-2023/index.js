4a638b27e380c1759366564208d1d88452e627c0
Kakon <pronobparsonal@gmail.com>
2023-02-05 12:12:10 +0600

    done

---
+++ 05-02-2023/index.js
@@ -0,0 +1,12 @@
+// 1-100 Number Sum
+
+function sum() {
+    let sum = 0;
+    for (let i = 1; i <= 10; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+const totalSum = sum ();
+console.log(totalSum);
